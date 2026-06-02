59b17e647b460c338ade22cb8b3d521c569ee04e
Mayank Pandey <mayankpandey0717gmail.com>
2026-06-02 12:16:35 +0530

    add

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -9,19 +9,23 @@ const edu = [
 
 export default function Education() {
   return (
-    <div className="mb-16 text-white ">
-      <h1 className="text-2xl font-bold text-white">
+    <div className="mb-16 text-white">
+      <h1 className="text-2xl font-bold">
         <span className="text-green-300">&gt;</span> education
       </h1>
+
       <div className="mt-10">
         {edu.map((ed) => (
           <div key={ed.title} className="mt-10 group">
             <a
               href={ed.link}
-              className="text-xl font-bold transition-all duration-300 ease-in-out group-hover:text-green-300"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-xl font-bold group-hover:text-green-300"
             >
               {ed.title}
             </a>
+
             <p className="text-gray-500 text-xs mt-2">
               {ed.position} | {ed.date}
             </p>
